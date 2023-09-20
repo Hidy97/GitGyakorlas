5025f9cc7b2a7c3fda32628d21ebc2c2a01af4f3
Hidy97 <csaladi.alexandra@gmail.com>
2023-09-20 12:50:45 +0200

    ciklus

--- src/gitgyakorlas/GitGyakorlas.java
+++ src/gitgyakorlas/GitGyakorlas.java
@@ -3,7 +3,9 @@ package gitgyakorlas;
 public class GitGyakorlas {
 
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++) {
+            
+        }
     }
     
 }
